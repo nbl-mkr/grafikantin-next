e9a42070e2fe07420bad95bda5dd2892ab9b0f47
nbl-mkr <n481lmaka@gmail.com>
2026-08-05 19:20:19 +0700

    change primary color

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -26,7 +26,7 @@ export default function HeroSection() {
           <div className="mt-8 flex justify-center md:justify-start">
             <a
               href="/order"
-              className="inline-flex items-center justify-center gap-2 rounded-lg bg-[#e76f51] hover:bg-[#d95d3f] px-7 py-3.5 text-sm font-semibold text-white shadow-lg shadow-[#E76F51]/25 transition-all duration-200 hover:opacity-90 hover:shadow-[#E76F51]/35 active:scale-[0.98]"
+              className="inline-flex items-center justify-center gap-2 rounded-lg bg-linear-to-r from-[#e76f51] to-[#f4a261] px-7 py-3.5 text-sm font-semibold text-white shadow-lg shadow-[#E76F51]/25 transition-all duration-200 hover:opacity-90 hover:shadow-[#E76F51]/35 active:scale-[0.98]"
             >
               <span>Pesan Sekarang</span>
             </a>
