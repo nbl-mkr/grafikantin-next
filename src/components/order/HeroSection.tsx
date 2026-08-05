d446a2c2ec1d87f8bffb8c41e889bf6d6023d0a8
nbl-mkr <n481lmaka@gmail.com>
2026-08-05 19:20:30 +0700

    change primary color

--- src/components/order/HeroSection.tsx
+++ src/components/order/HeroSection.tsx
@@ -12,7 +12,7 @@ export default function HeroSection() {
       <div className="relative mx-auto flex max-w-6xl flex-col items-start justify-between gap-8 px-6 md:flex-row md:items-center">
         <div className="w-full text-center md:text-left md:w-8/12">
           <h1 className="text-4xl font-extrabold text-gray-900 sm:text-5xl leading-tight">
-            Mau Makan Apa <span className="italic text-[#f97316]">Hari Ini?</span>
+            Mau Makan Apa <span className="italic text-[#e76f51]">Hari Ini?</span>
           </h1>
 
           <p className="mt-3 text-base italic text-gray-600">
@@ -21,15 +21,15 @@ export default function HeroSection() {
 
           <div className="mt-4 flex flex-wrap items-center justify-center md:justify-start gap-3 text-xs font-medium text-gray-500">
             <span className="flex items-center gap-1.5">
-              <span className="size-1.5 rounded-full bg-[#f97316]"></span>
+              <span className="size-1.5 rounded-full bg-[#e76f51]"></span>
               Penyajian Cepat
             </span>
             <span className="flex items-center gap-1.5">
-              <span className="size-1.5 rounded-full bg-[#f97316]"></span>
+              <span className="size-1.5 rounded-full bg-[#e76f51]"></span>
               Bebas Antre
             </span>
             <span className="flex items-center gap-1.5">
-              <span className="size-1.5 rounded-full bg-[#f97316]"></span>
+              <span className="size-1.5 rounded-full bg-[#e76f51]"></span>
               Menu Variatif
             </span>
           </div>
@@ -38,7 +38,7 @@ export default function HeroSection() {
         <div className="flex w-full justify-center md:w-auto md:justify-start">
           <a
             href="#cards"
-            className="inline-flex items-center justify-center gap-2 rounded-lg bg-linear-to-r from-[#f97316] to-[#fb923c] px-7 py-3.5 text-sm font-semibold text-white shadow-lg shadow-indigo-500/25 transition-all duration-200 hover:opacity-95 hover:shadow-indigo-500/35 active:scale-[0.98]"
+            className="inline-flex items-center justify-center gap-2 rounded-lg bg-linear-to-r from-[#e76f51] to-[#f4a261] px-7 py-3.5 text-sm font-semibold text-white shadow-lg shadow-indigo-500/25 transition-all duration-200 hover:opacity-95 hover:shadow-indigo-500/35 active:scale-[0.98]"
           >
             <span>Pesan Di Sini</span>
           </a>
