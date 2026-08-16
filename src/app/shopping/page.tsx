dea01a7e808c8ba0b8ee431e84aa9a8e00560257
nbl-mkr <n481lmaka@gmail.com>
2026-08-16 12:07:37 +0700

    fix shopping page

--- src/app/shopping/page.tsx
+++ src/app/shopping/page.tsx
@@ -2,8 +2,8 @@ import CartCard from "@/components/shopping/CartCard";
 
 export default function Shopping() {
     return (
-        <>
+        <div className="grow bg-slate-50">
           <CartCard />
-        </>
+        </div>
     );
 }
