5227e2a135b9ee231f493bcbef34fa6950552c64
nbl-mkr <n481lmaka@gmail.com>
2026-08-16 14:37:18 +0700

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <body className="flex min-h-screen flex-col">
         <CartProvider>
           <Navbar />
-          <main className="flex-1">{children}</main>
+          <main className="flex-1 bg-[#fafafa]">{children}</main>
           <Footer />
         </CartProvider>
       </body>
