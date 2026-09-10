50cceb7663a593865b7d58c09423f9f01f82fb35
nbl-mkr <n481lmaka@gmail.com>
2026-09-10 18:14:26 +0700

    update menu page

--- src/components/menu/MenuCard.tsx
+++ src/components/menu/MenuCard.tsx
@@ -19,13 +19,13 @@ interface MenuCardProps {
 
 export default function MenuCard({ item }: MenuCardProps) {
   return (
-    <div className="group rounded-lg bg-white p-4 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 border border-gray-100 flex flex-col justify-between">
+    <div className="group rounded-lg bg-white p-4 shadow-sm hover:shadow-[0_0_16px_rgba(15,23,42,0.05)] hover:-translate-y-1 transition-all duration-300 border border-gray-100 flex flex-col justify-between">
       <div>
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={item.gambar}
             alt={item.nama_menu}
-            className="h-52 w-full object-cover group-hover:scale-105 transition-transform duration-300"
+            className="h-52 w-full object-cover"
           />
         </div>
 
