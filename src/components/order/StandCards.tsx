4b952097056e1f7eecf589dea1adde234ad2a509
nbl-mkr <n481lmaka@gmail.com>
2026-09-10 18:12:07 +0700

    update order page

--- src/components/order/StandCards.tsx
+++ src/components/order/StandCards.tsx
@@ -28,7 +28,7 @@ export default function StandCards({ stands }: StandCardsProps) {
           {stands.map((stand) => (
             <div
               key={stand.id}
-              className="group w-full rounded-lg bg-white p-4 shadow-sm hover:shadow-xl hover:-translate-y-1 transition-all duration-300 border border-gray-100 flex flex-col justify-between"
+              className="group w-full rounded-lg bg-white p-4 shadow-sm hover:shadow-[0_0_16px_rgba(15,23,42,0.05)] hover:-translate-y-1 transition-all duration-300 border border-gray-100 flex flex-col justify-between"
             >
               <div>
                 <div className="relative overflow-hidden rounded-lg">
@@ -37,7 +37,7 @@ export default function StandCards({ stands }: StandCardsProps) {
                     alt={stand.nama_stand}
                     width={400}
                     height={208}
-                    className="h-52 w-full object-cover group-hover:scale-105 transition-transform duration-300 block"
+                    className="h-52 w-full object-cover block"
                   />
                 </div>
 
