39f2388909fa14ec2f1dd6ceedddf553ccc105f6
nbl-mkr <n481lmaka@gmail.com>
2026-09-14 10:14:39 +0700

    update home page

--- src/components/home/PopularMenu.tsx
+++ src/components/home/PopularMenu.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import AdaptiveImage from "@/components/common/AdaptiveImage";
 import ComplaintBanner from "@/components/home/ComplaintBanner";
 
 export interface PopularMenuItem {
@@ -76,11 +77,14 @@ export default function PopularMenu({ items, onSelect }: PopularMenuProps) {
           {featured && (
             <div className="group rounded-lg bg-white p-4 shadow-sm hover:shadow-[0_0_16px_rgba(15,23,42,0.05)] hover:-translate-y-1 transition-all duration-300 border border-gray-100 flex flex-col justify-between w-full max-w-[352px] mx-auto lg:justify-self-end lg:mx-0">
               <div>
-                <div className="relative overflow-hidden rounded-lg">
-                  <img
+                <div className="relative h-52 overflow-hidden rounded-lg">
+                  <AdaptiveImage
                     src={featured.gambar}
                     alt={featured.nama_menu}
-                    className="h-52 w-full object-cover"
+                    fill
+                    sizes="(max-width: 1024px) 100vw, 352px"
+                    className="object-cover"
+                    preload
                   />
                 </div>
 
